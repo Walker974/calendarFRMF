da505fb3f8df43d6eff4d9188acba0620cbc3dd2
Walker974 <idrisscherki739@gmail.com>
2025-06-04 10:20:29 +0100

    create organizer type repo

---
+++ src/main/java/org/calendar/dao/OrganizerTypeRepository.java
@@ -0,0 +1,7 @@
+package org.calendar.dao;
+
+import org.calendar.entities.OrganizerType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizerTypeRepository extends JpaRepository<OrganizerType, Long> {
+}
