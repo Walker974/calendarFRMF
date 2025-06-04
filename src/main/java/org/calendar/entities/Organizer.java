967858fa6445cca21d61efd806a473988faf892c
Walker974 <idrisscherki739@gmail.com>
2025-06-04 10:19:16 +0100

    remove name attribute

--- src/main/java/org/calendar/entities/Organizer.java
+++ src/main/java/org/calendar/entities/Organizer.java
@@ -14,7 +14,6 @@ public class Organizer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
     private String email;
     private String phoneNumber;
     @ManyToOne
