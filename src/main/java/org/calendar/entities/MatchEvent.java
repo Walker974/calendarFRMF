ca8f66a772d818de322b813ca9aa3b80eb1c22e7
Walker974 <idrisscherki739@gmail.com>
2025-06-10 15:32:37 +0100

    create match event entity

---
+++ src/main/java/org/calendar/entities/MatchEvent.java
@@ -0,0 +1,25 @@
+package org.calendar.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "match_event")
+@PrimaryKeyJoinColumn(name = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder
+public class MatchEvent extends StadiumEvent {
+
+    @ManyToOne
+    @JoinColumn(name = "home_team_id", nullable = false)
+    private Team homeTeam;
+
+    @ManyToOne
+    @JoinColumn(name = "away_team_id", nullable = false)
+    private Team awayTeam;
+}
