1647da1a17e3df7908e93350dadde136f166fa5e
Walker974 <idrisscherki739@gmail.com>
2025-06-13 14:50:15 +0100

    inject event type mapper

--- src/main/java/org/calendar/controllers/EventTypeController.java
+++ src/main/java/org/calendar/controllers/EventTypeController.java
@@ -3,7 +3,7 @@ package org.calendar.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.calendar.dto.EventTypeDto;
-import org.calendar.mappers.EventTypeConverter;
+import org.calendar.mappers.EventTypeMapper;
 import org.calendar.services.EventTypeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,18 +18,19 @@ import java.util.List;
 public class EventTypeController {
 
     private final EventTypeService eventTypeService;
+    private final EventTypeMapper eventTypeMapper;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @Operation(summary = "Get all event types", description = "Retrieve a list of all event types")
     public ResponseEntity<List<EventTypeDto>> getAllEventTypes() {
-        List<EventTypeDto> eventTypes = EventTypeConverter.toDtoList(eventTypeService.getAllEventTypes());
+        List<EventTypeDto> eventTypes = eventTypeMapper.toDtoList(eventTypeService.getAllEventTypes());
         return ResponseEntity.status(HttpStatus.OK).body(eventTypes);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
     @Operation(summary = "Create a new event type", description = "Create a new event type with the provided details")
     public ResponseEntity<EventTypeDto> createEventType(@RequestBody EventTypeDto eventTypeDto) {
-        EventTypeDto createdEventType = EventTypeConverter.toDto(eventTypeService.createEventType(EventTypeConverter.toEntity(eventTypeDto)));
+        EventTypeDto createdEventType = eventTypeMapper.toDto(eventTypeService.createEventType(eventTypeMapper.toEntity(eventTypeDto)));
         return ResponseEntity.status(HttpStatus.CREATED).body(createdEventType);
     }
 }
