2f811843e392ccd0d675449da9d86d59a8e7fdd3
Walker974 <idrisscherki739@gmail.com>
2025-06-13 14:50:39 +0100

    inject stadium mapper

--- src/main/java/org/calendar/controllers/StadiumController.java
+++ src/main/java/org/calendar/controllers/StadiumController.java
@@ -3,7 +3,7 @@ package org.calendar.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.calendar.dto.StadiumDto;
-import org.calendar.mappers.StadiumConverter;
+import org.calendar.mappers.StadiumMapper;
 import org.calendar.services.StadiumService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,18 +17,19 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 public class StadiumController {
     private final StadiumService stadiumService;
+    private final StadiumMapper stadiumMapper;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @Operation(summary = "Get all stadiums", description = "Retrieve a list of all stadiums")
     public ResponseEntity<List<StadiumDto>> getAllStadiums() {
-        List<StadiumDto> stadiums = StadiumConverter.toDtoList(stadiumService.getAllStadiums());
+        List<StadiumDto> stadiums = stadiumMapper.toDtoList(stadiumService.getAllStadiums());
         return ResponseEntity.status(HttpStatus.OK).body(stadiums);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
     @Operation(summary = "Create a new stadium", description = "Create a new stadium with the provided details")
     public ResponseEntity<?> createStadium(@RequestBody StadiumDto stadium) {
-        StadiumDto created = StadiumConverter.toDto(stadiumService.createStadium(StadiumConverter.toEntity(stadium)));
+        StadiumDto created = stadiumMapper.toDto(stadiumService.createStadium(stadiumMapper.toEntity(stadium)));
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 }
