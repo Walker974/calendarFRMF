3e37e693cc17be490ec28a2a3f53bef435983775
Walker974 <idrisscherki739@gmail.com>
2025-06-13 14:50:10 +0100

    inject competition mapper

--- src/main/java/org/calendar/controllers/CompetitionController.java
+++ src/main/java/org/calendar/controllers/CompetitionController.java
@@ -2,6 +2,7 @@ package org.calendar.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.calendar.mappers.CompetitionMapper;
 import org.calendar.services.CompetitionService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import java.util.List;
 import org.calendar.dto.CompetitionDto;
-import org.calendar.mappers.CompetitionConverter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,25 +22,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class CompetitionController {
 
     private final CompetitionService competitionService;
+    private final CompetitionMapper competitionMapper;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @Operation(summary = "Get all competitions", description = "Retrieve a list of all competitions")
     public ResponseEntity<List<CompetitionDto>> getAllCompetitions() {
-        List<CompetitionDto> competitions = CompetitionConverter.toDtoList(competitionService.getAllCompetitions());
+        List<CompetitionDto> competitions = competitionMapper.toDtoList(competitionService.getAllCompetitions());
         return ResponseEntity.status(HttpStatus.OK).body(competitions);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
     @Operation(summary = "Create a new competition", description = "Create a new competition with the provided details")
     public ResponseEntity<CompetitionDto> createCompetition(@RequestBody CompetitionDto competitionDto) {
-        CompetitionDto createdCompetition = CompetitionConverter.toDto(competitionService.createCompetition(CompetitionConverter.toEntity(competitionDto)));
+        CompetitionDto createdCompetition = competitionMapper.toDto(competitionService.createCompetition(competitionMapper.toEntity(competitionDto)));
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCompetition);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @Operation(summary = "Get competition by ID", description = "Retrieve a competition by its ID")
     public ResponseEntity<CompetitionDto> getCompetitionById(@PathVariable Long id) {
-        CompetitionDto competition = CompetitionConverter.toDto(competitionService.getCompetitionById(id));
+        CompetitionDto competition = competitionMapper.toDto(competitionService.getCompetitionById(id));
         return ResponseEntity.status(HttpStatus.OK).body(competition);
     }
 
