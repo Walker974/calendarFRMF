ef68bc27a117f1cf674128b87bec003eb458d525
Walker974 <idrisscherki739@gmail.com>
2025-06-13 09:41:36 +0100

    create competition dto

---
+++ src/main/java/org/calendar/dto/CompetitionDto.java
@@ -0,0 +1,9 @@
+package org.calendar.dto;
+
+import jdk.jshell.Snippet;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+public record CompetitionDto(Long id, String name) {
+}
