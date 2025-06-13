9fa5fe2d559c7da8576ab4c119ff2aab25bdf1ff
Walker974 <idrisscherki739@gmail.com>
2025-06-13 09:44:46 +0100

    remove unused imports

--- src/main/java/org/calendar/dto/CompetitionDto.java
+++ src/main/java/org/calendar/dto/CompetitionDto.java
@@ -1,9 +1,4 @@
 package org.calendar.dto;
 
-import jdk.jshell.Snippet;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
 public record CompetitionDto(Long id, String name) {
 }
