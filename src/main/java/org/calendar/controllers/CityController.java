da7b7b6b340cfbf820b5abc6d39e12da4416e912
Walker974 <idrisscherki739@gmail.com>
2025-06-13 14:50:04 +0100

    inject city mapper

--- src/main/java/org/calendar/controllers/CityController.java
+++ src/main/java/org/calendar/controllers/CityController.java
@@ -3,7 +3,7 @@ package org.calendar.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.calendar.dto.CityDto;
-import org.calendar.mappers.CityConverter;
+import org.calendar.mappers.CityMapper;
 import org.calendar.services.CityService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,18 +18,19 @@ import java.util.List;
 public class CityController {
 
     private final CityService cityService;
+    private final CityMapper cityMapper;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @Operation(summary = "Get all cities", description = "Retrieve a list of all cities")
     public ResponseEntity<List<CityDto>> getAllCities() {
-        List<CityDto> cities = CityConverter.toDtoList(cityService.getAllCities());
+        List<CityDto> cities = cityMapper.toDtoList(cityService.getAllCities());
         return ResponseEntity.status(HttpStatus.OK).body(cities);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
     @Operation(summary = "Create a new city", description = "Create a new city with the provided details")
     public ResponseEntity<CityDto> createCity(@RequestBody CityDto cityDto) {
-        CityDto createdCity = CityConverter.toDto(cityService.createCity(CityConverter.toEntity(cityDto)));
+        CityDto createdCity = cityMapper.toDto(cityService.createCity(cityMapper.toEntity(cityDto)));
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCity);
     }
 }
