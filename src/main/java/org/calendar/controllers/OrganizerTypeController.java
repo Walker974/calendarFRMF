0417d5a24aee7bbc0d9f8fba1098d6b730be6a39
Walker974 <idrisscherki739@gmail.com>
2025-06-13 14:50:28 +0100

    inject organizer type mapper

--- src/main/java/org/calendar/controllers/OrganizerTypeController.java
+++ src/main/java/org/calendar/controllers/OrganizerTypeController.java
@@ -3,7 +3,7 @@ package org.calendar.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.calendar.dto.OrganizerTypeDto;
-import org.calendar.mappers.OrganizerTypeConverter;
+import org.calendar.mappers.OrganizerTypeMapper;
 import org.calendar.services.OrganizerTypeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,25 +18,26 @@ import java.util.List;
 public class OrganizerTypeController {
 
     private final OrganizerTypeService organizerTypeService;
+    private final OrganizerTypeMapper organizerTypeMapper;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @Operation(summary = "Get all organizer types", description = "Retrieve a list of all organizer types")
     public ResponseEntity<List<OrganizerTypeDto>> getAllOrganizerTypes() {
-        List<OrganizerTypeDto> organizerTypes = OrganizerTypeConverter.toDtoList(organizerTypeService.getAllOrganizerTypes());
+        List<OrganizerTypeDto> organizerTypes = organizerTypeMapper.toDtoList(organizerTypeService.getAllOrganizerTypes());
         return ResponseEntity.status(HttpStatus.OK).body(organizerTypes);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @Operation(summary = "Get organizer type by ID", description = "Retrieve an organizer type by its ID")
     public ResponseEntity<OrganizerTypeDto> getOrganizerTypeById(@PathVariable Long id) {
-        OrganizerTypeDto organizerType = OrganizerTypeConverter.toDto(organizerTypeService.getOrganizerTypeById(id));
+        OrganizerTypeDto organizerType = organizerTypeMapper.toDto(organizerTypeService.getOrganizerTypeById(id));
         return ResponseEntity.status(HttpStatus.OK).body(organizerType);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
     @Operation(summary = "Create a new organizer type", description = "Add a new organizer type to the system")
     public ResponseEntity<OrganizerTypeDto> createOrganizerType(OrganizerTypeDto organizerTypeDto) {
-        OrganizerTypeDto createdOrganizerType = OrganizerTypeConverter.toDto(organizerTypeService.createOrganizerType(OrganizerTypeConverter.toEntity(organizerTypeDto)));
+        OrganizerTypeDto createdOrganizerType = organizerTypeMapper.toDto(organizerTypeService.createOrganizerType(organizerTypeMapper.toEntity(organizerTypeDto)));
         return ResponseEntity.status(HttpStatus.CREATED).body(createdOrganizerType);
     }
 }
