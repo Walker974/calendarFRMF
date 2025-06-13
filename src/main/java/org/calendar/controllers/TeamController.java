7667410ea7d1cf9e344ffddaaa42e1ccf114eeab
Walker974 <idrisscherki739@gmail.com>
2025-06-13 14:50:52 +0100

    inject team mapper

--- src/main/java/org/calendar/controllers/TeamController.java
+++ src/main/java/org/calendar/controllers/TeamController.java
@@ -3,7 +3,7 @@ package org.calendar.controllers;
 import lombok.RequiredArgsConstructor;
 import org.calendar.services.TeamService;
 import org.calendar.dto.TeamDto;
-import org.calendar.mappers.TeamConverter;
+import org.calendar.mappers.TeamMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,18 +17,19 @@ import java.util.List;
 public class TeamController {
 
     private final TeamService teamService;
+    private final TeamMapper teamMapper;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @Operation(summary = "Get all teams", description = "Retrieve a list of all teams")
     public ResponseEntity<List<TeamDto>> getAllTeams() {
-        List<TeamDto> teams = TeamConverter.toDtoList(teamService.getAllTeams());
+        List<TeamDto> teams = teamMapper.toDtoList(teamService.getAllTeams());
         return ResponseEntity.status(HttpStatus.OK).body(teams);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
     @Operation(summary = "Create a new team", description = "Create a new team with the provided details")
     public TeamDto createTeam(@RequestBody TeamDto teamDto) {
-        return TeamConverter.toDto(teamService.createTeam(TeamConverter.toEntity(teamDto)));
+        return teamMapper.toDto(teamService.createTeam(teamMapper.toEntity(teamDto)));
     }
 
 }
