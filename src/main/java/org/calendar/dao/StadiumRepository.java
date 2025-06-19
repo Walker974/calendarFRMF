123650b7669efbbf896dfcaa68c33c52a91e86c1
Walker974 <idrisscherki739@gmail.com>
2025-06-19 13:40:09 +0100

    create stadium repository

--- src/main/java/org/calendar/dao/StadiumRepository.java
+++ src/main/java/org/calendar/dao/StadiumRepository.java
@@ -2,6 +2,11 @@ package org.calendar.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.calendar.entities.Stadium;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StadiumRepository extends JpaRepository<Stadium, Long> {
+    @Query("SELECT s FROM Stadium s WHERE s.city.id = ?1")
+    List<Stadium> findByCityId(Long cityId);
 }
