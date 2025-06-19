3dd67a294d433b1b0422c327d3026724611f4a3b
Walker974 <idrisscherki739@gmail.com>
2025-06-19 13:39:18 +0100

    create Login Response Dto

---
+++ src/main/java/org/calendar/dto/LoginResponseDto.java
@@ -0,0 +1,8 @@
+package org.calendar.dto;
+
+import lombok.Builder;
+import org.calendar.enums.Role;
+
+@Builder
+public record LoginResponseDto(String token, String firstName, String lastName, String email, Role role, Long organizerId) {
+}
