3011359aacfb39c1aa87754042dcc50b3ed8ffb4
Walker974 <idrisscherki739@gmail.com>
2025-06-19 13:36:30 +0100

    add update & delete city services

--- src/main/java/org/calendar/services/CityService.java
+++ src/main/java/org/calendar/services/CityService.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CityService {
     private final CityRepository cityRepository;
+    private final StadiumService stadiumService;
 
     public List<City> getAllCities() {
         return cityRepository.findAll();
@@ -18,6 +19,31 @@ public class CityService {
 
     public City createCity(City city) {
         return cityRepository.save(city);
+    }
+
+    public City getCityById(Long id) {
+        return cityRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("City not found with id: " + id));
+    }
+
+    public City updateCity(Long cityId, City city) {
+        try {
+            City existingCity = getCityById(cityId);
+            existingCity.setName(city.getName());
+            return cityRepository.save(existingCity);
+        } catch (RuntimeException e) {
+            throw new IllegalArgumentException("Invalid city ID: " + cityId, e);
+        }
+    }
+
+    public void deleteCity(Long cityId) {
+        City city = cityRepository.findById(cityId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid city ID: " + cityId));
+
+        if (!stadiumService.getStadiumsByCityId(cityId).isEmpty()) {
+            throw new IllegalArgumentException("Cannot delete city with associated stadiums");
+        }
+        cityRepository.delete(city);
     }
 }
 
